ee3a56d7b1ef265189835d9f66e8955e5616788e
victor charl <corpuzvictorcharl@gmail.com>
2020-04-26 22:38:50 -0600

    initial commit

--- app/src/main/java/com/victorcharl/tictactoegame/MainActivity.java
+++ app/src/main/java/com/victorcharl/tictactoegame/MainActivity.java
@@ -7,89 +7,74 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    String players = "oPlayer";
-    int totalMoves = 9;
+    private String players = "PLAYER O TURNS";
+    private int totalMoves = 9;
 
-    String winner = "";
+    private String winner = "";
 
-    ImageView iv1, iv2, iv3 ,iv4, iv5 ,iv6 ,iv7 ,iv8 ,iv9;
+    private ImageView iv1, iv2, iv3 ,iv4, iv5 ,iv6 ,iv7 ,iv8 ,iv9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        iv1 = (ImageView)findViewById(R.id.imageView1);
-        iv2 = (ImageView)findViewById(R.id.imageView2);
-        iv3 = (ImageView)findViewById(R.id.imageView3);
-        iv4 = (ImageView)findViewById(R.id.imageView4);
-        iv5 = (ImageView)findViewById(R.id.imageView5);
-        iv6 = (ImageView)findViewById(R.id.imageView6);
-        iv7 = (ImageView)findViewById(R.id.imageView7);
-        iv8 = (ImageView)findViewById(R.id.imageView8);
-        iv9 = (ImageView)findViewById(R.id.imageView9);
+        iv1 = findViewById(R.id.imageView1);
+        iv2 = findViewById(R.id.imageView2);
+        iv3 = findViewById(R.id.imageView3);
+        iv4 = findViewById(R.id.imageView4);
+        iv5 = findViewById(R.id.imageView5);
+        iv6 = findViewById(R.id.imageView6);
+        iv7 = findViewById(R.id.imageView7);
+        iv8 = findViewById(R.id.imageView8);
+        iv9 = findViewById(R.id.imageView9);
 
     }
 
     public void placeYourLogo(View view) {
         ImageView playerLogo = (ImageView)view;
-        if(players.equals("oPlayer"))
+        if(players.equals("PLAYER O TURNS"))
         {
             playerLogo.setImageResource(R.drawable.o_player);
             playerLogo.setClickable(false);
             checkWhoIsWinning();
-            players = "xPlayer";
+            players = "PLAYER X TURNS";
         }
         else
         {
             playerLogo.setImageResource(R.drawable.x_player);
             playerLogo.setEnabled(false);
             checkWhoIsWinning();
-            players = "oPlayer";
+            players = "PLAYER O TURNS";
         }
         totalMoves--;
         playerLogo.animate().scaleX(0.7f).scaleY(0.7f);
     }
 
-    public void checkWhoIsWinning(){
-        /*Winning Combinations
-        * { 123 , 456 , 789 } vertical win
-        * { 147 , 258 , 369 } horizontal win
-        * { 159 , 357 } diagonal win*/
-        //Conditions for Player O
-        if((!(iv1.isClickable()) && !(iv2.isClickable()) && !(iv3.isClickable())) ||
-                (!(iv4.isClickable()) && !(iv5.isClickable()) && !(iv6.isClickable())) ||
-                (!(iv7.isClickable()) && !(iv8.isClickable()) && !(iv9.isClickable())) ||
-                (!(iv1.isClickable()) && !(iv4.isClickable()) && !(iv7.isClickable())) ||
-                (!(iv2.isClickable()) && !(iv5.isClickable()) && !(iv8.isClickable())) ||
-                (!(iv3.isClickable()) && !(iv6.isClickable()) && !(iv9.isClickable())) ||
-                (!(iv1.isClickable()) && !(iv5.isClickable()) && !(iv9.isClickable())) ||
-                (!(iv3.isClickable()) && !(iv5.isClickable()) && !(iv7.isClickable())))
-            {
+    private void checkWhoIsWinning(){
+        ImageView[] imageViews = new ImageView[]{null, iv1,iv2,iv3,iv4,iv5,iv6,iv7,iv8,iv9};
+        Integer[][] winningCombinations = new Integer[][]{{1,2,3}, {4,5,6}, {7,8,9}, {1,4,7}, {2,5,8}, {3,6,9}, {1,5,9}, {3,5,7}};
+
+        for (Integer[] winningCombination : winningCombinations) {
+            if(!(imageViews[winningCombination[0]].isClickable()) && !(imageViews[winningCombination[1]].isClickable()) &&
+                    !(imageViews[winningCombination[2]].isClickable())) {
                 winner = "O";
                 showOutcomeDialog();
             }
-        //Conditions for Player X
-        if((!(iv1.isEnabled()) && !(iv2.isEnabled()) && !(iv3.isEnabled())) ||
-                (!(iv4.isEnabled()) && !(iv5.isEnabled()) && !(iv6.isEnabled())) ||
-                (!(iv7.isEnabled()) && !(iv8.isEnabled()) && !(iv9.isEnabled())) ||
-                (!(iv1.isEnabled()) && !(iv4.isEnabled()) && !(iv7.isEnabled())) ||
-                (!(iv2.isEnabled()) && !(iv5.isEnabled()) && !(iv8.isEnabled())) ||
-                (!(iv3.isEnabled()) && !(iv6.isEnabled()) && !(iv9.isEnabled())) ||
-                (!(iv1.isEnabled()) && !(iv5.isEnabled()) && !(iv9.isEnabled())) ||
-                (!(iv3.isEnabled()) && !(iv5.isEnabled()) && !(iv7.isEnabled())))
-            {
+            else if(!(imageViews[winningCombination[0]].isEnabled()) && !(imageViews[winningCombination[1]].isEnabled()) &&
+                    !(imageViews[winningCombination[2]].isEnabled())) {
                 winner = "X";
                 showOutcomeDialog();
             }
+        }
+
         if(totalMoves == 1)
             showOutcomeDialog();
     }
 
-    public void showOutcomeDialog(){
+    private void showOutcomeDialog(){
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         if(winner.equals(""))
             dialog.setTitle("DRAW");
